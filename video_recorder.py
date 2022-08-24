78d4cc657100a3ec19de4230049ed9d2658029ba
Atick Faisal <atickfaisal@gmail.com>
2022-08-24 13:04:09 +0300

    Implement basic video reocding

--- video_recorder.py
+++ video_recorder.py
@@ -4,22 +4,31 @@ import cv2
 cap = cv2.VideoCapture(0)
 fps = cap.get(cv2.CAP_PROP_FPS)
 
+print("Initializing ... ", end="")
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer = cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(
+writer = cv2.VideoWriter('dataset/basicvideo.mp4', cv2.VideoWriter_fourcc(
     *'mp4v'), 30.0, (width, height))
 
-
+count = 0
 n_frames = 0
 total_frames = 5 * 30
+current_time = time.time()
+
+print("OK")
 
 while n_frames < total_frames:
     ret, frame = cap.read()
 
     writer.write(frame)
     n_frames = n_frames + 1
+
+    if time.time() > current_time + 1.0:
+        current_time = time.time()
+        count = count + 1
+        print(count)
 
     # cv2.imshow('frame', frame)
 
