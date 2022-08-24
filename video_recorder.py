766b7a509a82b071bd3537d0f6b07e1c04014b39
Atick Faisal <atickfaisal@gmail.com>
2022-08-24 12:57:33 +0300

    Refacotr

--- video_recorder.py
+++ video_recorder.py
@@ -1,21 +1,22 @@
 import time
 import cv2
 
-cap= cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 fps = cap.get(cv2.CAP_PROP_FPS)
 
 
-width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30.0, (width,height))
+writer = cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(
+    *'mp4v'), 30.0, (width, height))
 
 
 n_frames = 0
 total_frames = 5 * 30
 
 while n_frames < total_frames:
-    ret,frame= cap.read()
+    ret, frame = cap.read()
 
     writer.write(frame)
     n_frames = n_frames + 1
