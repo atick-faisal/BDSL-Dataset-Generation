ddbd43eb720d5a095717e991a67b44b4f5d60a12
Atick Faisal <atickfaisal@gmail.com>
2022-08-25 07:44:19 +0300

    Serve react app from flask

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, send_from_directory
+
 from video_recorder import (
     record_data,
     delete_last_recording,
     cleanup
 )
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="frontend/build")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/record', methods=["POST"])
