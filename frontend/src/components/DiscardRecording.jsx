bd38363c70d260ad48c07423947dc5fa3e4fd81a
Atick Faisal <atickfaisal@gmail.com>
2022-08-25 07:45:25 +0300

    Refactor

--- frontend/src/components/DiscardRecording.jsx
+++ frontend/src/components/DiscardRecording.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 
 export default function DiscardRecording({ onDiscardRecording }) {
     return (
